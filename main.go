eb8d7ca446260ee8d08356ab5538fa1c0b47e4ec
KaanTanis <kt@kaantanis.com>
2025-12-06 11:47:20 +0300

    wip

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 	"time"
 
 	"gopkg.in/yaml.v2"
@@ -33,52 +37,77 @@ func loadConfig(path string) (*Config, error) {
 	return &cfg, nil
 }
 
-func checkUrl(target TargetConfig) {
-	for {
-		processRequest(target)
-
-		time.Sleep(target.Interval)
-	}
-}
-
 func processRequest(target TargetConfig) {
 	start := time.Now()
 	client := http.Client{
-		Timeout: 15 * time.Second,
+		Timeout: 10 * time.Second,
 	}
 
 	resp, err := client.Get(target.URL)
 	if err != nil {
-		elapsed := time.Since(start)
-
-		fmt.Printf("[ERR] Target: %s - URL: %s - Error %s - Time: %v\n", target.Name, target.URL, err.Error(), elapsed)
+		fmt.Printf("[ERR] Target: %s - Error: %s\n", target.Name, err)
 		return
 	}
 
 	defer resp.Body.Close()
-	
-	elapsed := time.Since(start)
 
 	status := "OK"
 	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
 		status = "WARN"
 	}
 
-	fmt.Printf("[LOG] Target: %s - URL: %s - Status: %s (%d) - Time: %v\n", target.Name, target.URL, status, resp.StatusCode, elapsed)
+	fmt.Printf("[LOG] Target: %s - Status: %s (%d) - Time: %v\n", target.Name, status, resp.StatusCode, time.Since(start))
+}
+
+func checkUrl(ctx context.Context, wg *sync.WaitGroup, target TargetConfig) {
+	defer wg.Done()
+
+	fmt.Printf("Started monitoring: %s\n", target.Name)
+
+	ticker := time.NewTicker(target.Interval)
+	defer ticker.Stop()
+
+	// first control
+	processRequest(target)
+
+	for {
+		select {
+			case <-ctx.Done():
+				fmt.Printf("Stopping monitor for: %s\n", target.Name)
+				return
+			case <-ticker.C:
+				processRequest(target)
+		}
+	}
 }
 
 func main() {
 	cfg, err := loadConfig("config.yaml")
 	if err != nil {
-		fmt.Printf("Fatal error loading config %v\n", err)
+		fmt.Printf("Fatal error: %v\n", err)
 		os.Exit(1)
 	}
 
-	fmt.Printf("Agent started, loaded %d targets.\n", len(cfg.Targets))
+	// listen close channels
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+
+	// cancalable context
+	ctx, cancel := context.WithCancel(context.Background())
+	var wg sync.WaitGroup
+
+	fmt.Printf("Agent started with %d targets. Press Ctrl+C to stop.\n", len(cfg.Targets))
 
 	for _, target := range cfg.Targets {
-		go checkUrl(target)
+		wg.Add(1)
+		go checkUrl(ctx, &wg, target)
 	}
 
-	select {}
+	<-sigChan
+	fmt.Println("\nShutdown signal received. Cleaning up...")
+
+	cancel()
+
+	fmt.Println("All workers stopped. Bye!")
+
 }
